e7c05f57680accd70818edcca605bc57b155b309
defitarani <112389956+defitarani@users.noreply.github.com>
2022-08-30 17:29:21 +0700

    Delete src/com/prodemy/h2 directory

--- src/com/prodemy/h2/TestScoping.java
+++
@@ -1,8 +0,0 @@
-package com.prodemy.h2;
-
-public class TestScoping {
-    public static void main(String[] args) {
-        ScopeExample scope = new ScopeExample();
-        scope.firstMethod();
-    }
-}
