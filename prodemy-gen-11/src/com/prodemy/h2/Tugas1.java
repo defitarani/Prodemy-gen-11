98bf90058ae412d0c129322d79f346c6966435e9
defitarani <112389956+defitarani@users.noreply.github.com>
2022-08-30 17:30:57 +0700

    Add files via upload

---
+++ prodemy-gen-11/src/com/prodemy/h2/Tugas1.java
@@ -0,0 +1,12 @@
+package com.prodemy.h2;
+
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String nama = sc.nextLine();
+        System.out.print("Hai " + nama + "! have a good day");
+    }
+}
